a37e5cd50f5c46cf68afe3eb3a8e13e02424c048
MiguelGuevara25 <miguel_g1789@hotmail.com>
2022-05-18 09:01:42 -0500

    new changed

--- js/06.js
+++ js/06.js
@@ -5,5 +5,12 @@ resultado = Math.PI;
 resultado = Math.round(2.6);
 resultado = Math.ceil(2.2);
 resultado = Math.floor(2.1);
+resultado = Math.sqrt(144);
+resultado = Math.abs(-200);
+resultado = Math.min( 3, 5, 1, 8, 2, 10);
+resultado = Math.max( 3, 5, 1, 8, 2, 10);
+resultado = Math.random();
+resultado = Math.floor(Math.random() * 100);
+
 
 console.log(resultado);
