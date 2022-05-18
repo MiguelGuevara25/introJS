89b923ffef4ccecd909505ff56cbb3f0e2394f39
MiguelGuevara25 <miguel_g1789@hotmail.com>
2022-05-18 00:06:12 -0500

    new changed

---
+++ js/04.js
@@ -0,0 +1,17 @@
+//String o cadenas de texto
+const tweet = 'Aprendiendo desarrollo web con un joto';
+const producto2 = 'Monitor HD';
+const email = 'correo@correo.com'
+
+//Para la extensión
+console.log(tweet.length);
+console.log(producto2);
+
+//IndexOf (retorna posición)
+console.log(tweet.indexOf('web'));
+console.log(producto2.indexOf('nose'));
+console.log(email.indexOf('@'));
+
+//Includes (retorna true o false)
+console.log(tweet.includes('web'));
+console.log(producto2.includes('nose'));
---
+++ js/06.js
@@ -0,0 +1,9 @@
+//Objeto MATH
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.6);
+resultado = Math.ceil(2.2);
+resultado = Math.floor(2.1);
+
+console.log(resultado);
